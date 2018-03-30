3eec5f6851ef0c136f8e6a234bde0294a1577a61
Peter MOUEZA <mouezapeter@gmail.com>
2018-03-30 21:19:49 +0200

    start

---
+++ cloudPivotal/src/main/java/cloudPivotal2/CloudPivotal2Application.java
@@ -0,0 +1,12 @@
+package cloudPivotal2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPivotal2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudPivotal2Application.class, args);
+	}
+}
---
+++ cloudPivotal/src/test/java/cloudPivotal2/CloudPivotal2ApplicationTests.java
@@ -0,0 +1,16 @@
+package cloudPivotal2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudPivotal2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
